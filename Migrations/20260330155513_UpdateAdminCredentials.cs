2edb9b5c77168cb23d24d71d767fbde228f8d830
FaskoKeca <vpishtialov.tds@gmail.com>
2026-03-30 18:59:22 +0300

    Added "Logout" button and made admin password "admin123"

---
+++ Migrations/20260330155513_UpdateAdminCredentials.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentACar3.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAdminCredentials : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: BCrypt.Net.BCrypt.HashPassword("admin123"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
